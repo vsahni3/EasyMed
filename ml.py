162bff4df23faabb57400c7b75b90732c1e2d365
Varun Sahni <varunsahni3504@gmail.com>
2022-11-05 12:54:01 -0400

    ml

--- ml.py
+++ ml.py
@@ -1,15 +1,38 @@
 import requests
 
 url = "https://freeocrapi.com/api"
-filename = "/Users/varunsahni/Desktop/myopd-sample-rx-eng.png"
+filename = "/Users/varunsahni/Desktop/JohnSmith-Example.jpg"
 data = {'file': open(filename, 'rb')}
 response = requests.request("POST", url, files=data)
-print(dict(response.json())['text'].replace('\n', ' '))
-# def extract_data(raw_data):
-#     data = {
-#         'name': '',
-#         'dosage': '',
-#         'timings': ''
-#     }
-#     for i range(len(raw_data)):
-#         if 
+data = dict(response.json())['text'].replace('\n', ' ')
+def get_data(filename):
+    url = "https://freeocrapi.com/api"
+    data = {'file': open(filename, 'rb')}
+    response = requests.request("POST", url, files=data)
+    data = dict(response.json())['text'].replace('\n', ' ')
+    return data
+
+def extract_data(filename):
+    raw_data = get_data(filename)
+    data = {
+        'names': [],
+        'dosages': []
+    }
+    for i in range(len(raw_data)):
+        if raw_data[i:i+2] == 'mg':
+            start_index = i - 2
+            cur_dosage = ''
+            while raw_data[start_index] != ' ':
+                cur_dosage += raw_data[start_index]
+                start_index -= 1
+            data['dosages'].append(cur_dosage[::-1])
+            start_index -= 1
+            cur_name = ''
+            while raw_data[start_index] != ' ':
+                cur_name += raw_data[start_index]
+                start_index -= 1
+            data['names'].append(cur_name[::-1])
+    
+    return data
+
+    
